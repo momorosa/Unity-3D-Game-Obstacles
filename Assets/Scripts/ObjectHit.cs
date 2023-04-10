fd363506a8e098ec52622596db5b8a37fbca1c95
Rosa Choi <momorosa.design@gmail.com>
2023-04-09 17:07:05 -0700

    Use "Player" tag to limit when behavior will occur such as obstacles turning another color upon collision

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -6,7 +6,10 @@ public class ObjectHit : MonoBehaviour
 {
     void OnCollisionEnter(Collision other)
     {
-        GetComponent<MeshRenderer>().material.color = Color.red;
-        
+        if(other.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+        } 
+
     }
 }
