3214e779b987af976a13cfbb81c0b637b1e619bd
Rosa Choi <momorosa.design@gmail.com>
2023-04-09 17:27:05 -0700

    Using tags and conditional statement, hit score only gets counted once when the player hits obstacles.

--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -8,7 +8,10 @@ public class Scorer : MonoBehaviour
 
     void OnCollisionEnter(Collision other)
     {
-        hits++;
-        Debug.Log("You've bumped into a thing this many times: " + hits);
+        if(other.gameObject.tag != "Ground" && other.gameObject.tag != "Hit")
+        {
+            hits++;
+            Debug.Log("You've bumped into a thing this many times: " + hits);
+        }
     }
 }
